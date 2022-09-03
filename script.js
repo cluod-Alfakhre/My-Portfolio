cc3380c3ef738bf09e820ec74728b2448dbc4410
cluod-Alfakhre <100804949+cluod-Alfakhre@users.noreply.github.com>
2022-09-03 20:29:00 +0200

    complete responsivnes and js

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ theme.forEach((el)=>{
     })
 })
 
+///////////////////////////////////
 
 const swiper = new Swiper('.swiper', {
     // Optional parameters
@@ -26,7 +27,50 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
-    // And if we need scrollbar
-    
+      
   });
+/////////////////////////////////////////////
+
+const nav = document.querySelectorAll('header li');
+
+const boxes = document.querySelectorAll('.box');
+
+nav.forEach(el => {
+    el.addEventListener('click',()=>{
+
+        nav.forEach(el => el.classList.remove('active'));
+
+        el.classList.add('active');
+        switchTo(el.innerText.toLowerCase());
+
+    })
+})  
+
+boxes.forEach(el => {
+    el.style.order = el.dataset.order;
+})
+
+function switchTo(selector){
+
+    boxes.forEach(el => {
+        if(el.classList.contains(selector)){
+            el.classList.remove('half-opacity');
+            el.classList.add('full-opacity');
+        }
+        
+        else if(selector === 'all'){
+            el.classList.remove('full-opacity');
+            el.classList.remove('half-opacity');
+        }
+        else if(selector === 'language'){
+            el.classList.remove('full-opacity');
+            el.classList.remove('half-opacity');
+        }
+
+        else{
+            el.classList.remove('full-opacity');
+            el.classList.add('half-opacity');
+        }
+    })
+
+}
